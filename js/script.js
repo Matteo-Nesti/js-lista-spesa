697da07017284274f5c99d1bb0ce18e4182f7a02
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-06-05 15:18:28 +0200

    add for and add elements on HTML

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ console.log('js ok')
 
 /* traccia Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che
 si vedano) gli elementi della lista individualmente con un ciclo while. */
-
+const container = document.querySelector('.container')
 const shopList = ['pane', 'latte', 'uova', 'tonno', 'riso', 'peperoncino'];
+
+// ciclato col for
 
 let shopListText = '';
 
 for(let i = 0; i < shopList.length; i++){
     shopListText += `<div>${shopList[i]}</div>`
 }
+
+container.innerHTML = shopListText;
+
